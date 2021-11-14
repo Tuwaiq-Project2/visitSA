100a0720a54900ea860efa10d53879ffd788e313
walaTuwaiq <wala.radi.rae.js@tuwaiq.edu.sa>
2021-11-14 20:29:19 +0300

    try

--- frontend/src/Component/NavBar.js
+++ frontend/src/Component/NavBar.js
@@ -1,11 +1,22 @@
 import React from 'react'
+import { Link } from "react-router-dom";
+import "./navBar.css"
 
 export default function NavBar() {
 
 
     return (
         <div>
-            NavBar
+            
+   <div className="mainCont">
+
+
+     <span><Link to="/">Home</Link>    </span>
+     <span><Link to="/header">Header</Link>  </span>
+     <span><Link to="/navbar">NavBar</Link>  </span>
+     <span><Link to="/mustToVisit">MustToVisit</Link>  </span>
+
+       </div>
         </div>
     )
 }
