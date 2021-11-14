98029160e1a46e9dc7e245fe038f3f5fe9d58df3
walaTuwaiq <wala.radi.rae.js@tuwaiq.edu.sa>
2021-11-14 11:10:11 +0300

    basic

---
+++ src/Component/Home.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import NavBar from './NavBar'
+import Header from './Header'
+import MustToVisit from './MustToVisit'
+
+export default function Home() {
+
+    return (
+        <div>
+            Home
+            <NavBar/>
+            <Header/>
+            <MustToVisit/>
+
+        </div>
+    )
+}
