fdd9c6ce3ace16fdb810bf22e92f7e92ac5ea2de
walaTuwaiq <wala.radi.rae.js@tuwaiq.edu.sa>
2021-11-15 09:48:25 +0300

    edit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 import Home from "./Component/Home";
-import { Route,BrowserRouter } from "react-router-dom";
+import { Route, BrowserRouter } from "react-router-dom";
 import Header from "./Component/Header";
 import MustToVisit from "./Component/MustToVisit";
 import NavBar from "./Component/NavBar";
@@ -8,18 +8,12 @@ function App() {
   return (
     <div>
       app
-      <Home/>
-      <NavBar/>
-      <Route exact path="/" component={Home}/>
+      <NavBar />
+      <Route exact path="/home" component={Home} />
       <Route exact path="/header" component={Header} />
       <Route exact path="/mustovisit" component={MustToVisit} />
-
-
-
     </div>
-
   );
 }
 
-export default App; 
-
+export default App;
