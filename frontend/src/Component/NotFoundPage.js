33f2d74559faa16eae5eb2f204a0d909d84acbae
walaTuwaiq <wala.radi.rae.js@tuwaiq.edu.sa>
2021-11-17 22:42:53 +0300

    added

---
+++ frontend/src/Component/NotFoundPage.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import "./NotFoundPage.css"
+import notFound from "./5203299.jpg"
+
+export default function NotFoundPage() {
+    return (
+        <div>
+            <img className="not-found-page" src={notFound} alt="" />
+        </div>
+    )
+}
